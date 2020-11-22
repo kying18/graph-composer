7ad03fe461648b03e0ced9b470365789abac7daa
Kylie Ying <kying@mit.edu>
2020-11-21 22:55:09 -0500

    Changed to be not just lyrics

--- compose.py
+++ compose.py
@@ -6,15 +6,20 @@ import random
 
 
 def get_words_from_text(text_path):
-    with open(text_path, 'r') as file:
-        text = file.read()
+    with open(text_path, 'rb') as file:
+        text = file.read().decode("utf-8") 
+
         # remove [verse 1: artist]
-        text = re.sub(r'\[(.+)\]', ' ', text)
+        # include the following line if you are doing song lyrics
+        # text = re.sub(r'\[(.+)\]', ' ', text)
+
         text = ' '.join(text.split())
         text = text.lower()
         text = text.translate(str.maketrans('', '', string.punctuation))
 
     words = text.split()
+
+    words = words[:1000]
 
     return words
 
@@ -35,31 +40,32 @@ def make_graph(words):
 
         prev_word = word_vertex
 
+    g.generate_probability_mappings()
+    
     return g
-
 
 def compose(g, words, length=50):
     composition = []
     word = g.get_vertex(random.choice(words))
     for _ in range(length):
         composition.append(word.value)
-        word = random.choice(word.probability_map)
+        word = g.get_next_word(word)
 
     return composition
 
 
-def main(artist):
-    words = []
-    for song in os.listdir('songs/{}'.format(artist)):
-        if song == '.DS_Store':
-            continue
-        words.extend(get_words_from_text('songs/{artist}/{song}'.format(artist=artist, song=song)))
+def main():
+    words = get_words_from_text('texts/hp_sorcerer_stone.txt')
+
+    # for song in os.listdir('songs/{}'.format(artist)):
+        # if song == '.DS_Store':
+        #     continue
+        # words.extend(get_words_from_text('songs/{artist}/{song}'.format(artist=artist, song=song)))
         
     g = make_graph(words)
-    g.generate_probability_mappings()
     composition = compose(g, words, 100)
     print(' '.join(composition))
 
 
 if __name__ == '__main__':
-    main('roddy_ricch')
+    main()
--- graph.py
+++ graph.py
@@ -1,8 +1,11 @@
+import random
+
 class Vertex(object):
     def __init__(self, value):
         self.value = value
         self.adjacent = {}  # nodes that it points to
-        self.probability_map = []
+        self.neighbors = []
+        self.neighbors_weights = []
 
     def __str__(self):
         return self.value + ' '.join([node.value for node in self.adjacent.keys()])
@@ -19,7 +22,12 @@ class Vertex(object):
     # initializes probability map
     def get_probability_map(self):
         for (vertex, weight) in self.adjacent.items():
-            self.probability_map.extend([vertex for _ in range(weight)])
+            self.neighbors.append(vertex)
+            self.neighbors_weights.append(weight)
+
+    def next_word(self):
+        return random.choices(self.neighbors, weights=self.neighbors_weights)[0]
+
 
 
 class Graph(object):
@@ -37,13 +45,9 @@ class Graph(object):
             self.add_vertex(value)
         return self.vertices[value]
 
+    def get_next_word(self, current_vertex):
+        return self.vertices[current_vertex.value].next_word()
+
     def generate_probability_mappings(self):
         for vertex in self.vertices.values():
             vertex.get_probability_map()
-
-    # def add_edge(self, start, end, weight=0):
-    #     # start and end are values of the nodes, not node objects
-    #     self.vertices[start].add_edge_to(self.vertices[end], weight)
-    #
-    # def increment_edge(self, start, end):
-    #     self.vertices[start]
