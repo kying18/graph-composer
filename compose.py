66d87409900d446c532373307d01d732de8cc0b0
FitriRibbit <91612800+FitriRibbit@users.noreply.github.com>
2022-10-07 06:04:57 -0700

    Update compose.py
    Add new function in order that all of function can work and made repeat program code

--- compose.py
+++ compose.py
@@ -10,19 +10,30 @@ Github: https://www.github.com/kying18
 Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5EUb3gzmlXIdsJkPs?si=qGc4ubKRRYmPHAJAIrCxVQ 
 """
 
+"""
+Empty Compose Template to implement :D
+
+YouTube Kylie Ying: https://www.youtube.com/ycubed 
+Twitch KylieYing: https://www.twitch.tv/kylieying 
+Twitter @kylieyying: https://twitter.com/kylieyying 
+Instagram @kylieyying: https://www.instagram.com/kylieyying/ 
+Website: https://www.kylieying.com
+Github: https://www.github.com/kying18 
+Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5EUb3gzmlXIdsJkPs?si=qGc4ubKRRYmPHAJAIrCxVQ 
+"""
+
 import os
 import re
 import string
 import random
+
 from graph import Graph, Vertex
 
 def get_words_from_text(text_path):
-    with open(text_path, 'rb') as file:
-        text = file.read().decode("utf-8") 
+    with open(text_path, 'rb') as f:
+        text = f.read().decode("utf-8")
 
-        # remove [verse 1: artist]
-        # include the following line if you are doing song lyrics
-        # text = re.sub(r'\[(.+)\]', ' ', text)
+        text = re.sub(r'\[(.+)\]', ' ', text)
 
         text = ' '.join(text.split())
         text = text.lower()
@@ -31,52 +42,77 @@ def get_words_from_text(text_path):
     words = text.split()
 
     words = words[:1000]
-
     return words
-
 
 def make_graph(words):
     g = Graph()
-    prev_word = None
-    # for each word
+    previous_word = None
+    
+
     for word in words:
-        # check that word is in graph, and if not then add it
         word_vertex = g.get_vertex(word)
 
-        # if there was a previous word, then add an edge if does not exist
-        # if exists, increment weight by 1
-        if prev_word:  # prev word should be a Vertex
-            # check if edge exists from previous word to current word
-            prev_word.increment_edge(word_vertex)
+        if previous_word:
+            previous_word.increment_edge(word_vertex)
 
-        prev_word = word_vertex
-
-    g.generate_probability_mappings()
+        previous_word = word_vertex
     
+    g.generate_probability_mappings()
     return g
 
+        
 def compose(g, words, length=50):
     composition = []
     word = g.get_vertex(random.choice(words))
     for _ in range(length):
         composition.append(word.value)
         word = g.get_next_word(word)
-
     return composition
 
+def main(artist):
+    words = []
 
-def main():
-    words = get_words_from_text('texts/hp_sorcerer_stone.txt')
+    for song_file in os.listdir(f'songs/{artist}'):
+        if song_file == '.DS_Store':
+            continue
+        song_words = get_words_from_text(f'songs/{artist}/{song_file}')
+        words.extend(song_words)
 
-    # for song in os.listdir('songs/{}'.format(artist)):
-        # if song == '.DS_Store':
-        #     continue
-        # words.extend(get_words_from_text('songs/{artist}/{song}'.format(artist=artist, song=song)))
-        
     g = make_graph(words)
-    composition = compose(g, words, 100)
-    print(' '.join(composition))
 
+    composition = compose(g, words, 100)
+    return ' '.join(composition)
+
+#made new function in order that all function can work
+def mainw():
+    words = get_words_from_text(choice+'/hp_sorcerer_stone.txt')
+    g = make_graph(words)
+
+    composition = compose(g, words, 100)
+    return ' '.join(composition)
 
 if __name__ == '__main__':
-    main()
+    #user input
+    choice = input('Select folder you wanna graph: texts or song ')
+    if choice == 'song':
+        artist = input('Put Your Artist Name: alan_warker, armin_van_buuren, avicii, billie_eilish, celine_dion, drake, green_day, halsey, Lady_Gaga, linkin_park, queen, taylor_swift ')
+        print(main(artist))
+    elif choice == 'texts':
+        print(mainw())
+    else:
+        quit()
+
+    #made replay game
+    while True:
+        again = input('Still wanna try? (Y/n) ')
+        if again == 'Y' or again == 'y':
+            choice = input('Select folder you wanna graph: texts or song ')
+            if choice == 'song':
+                artist = input('Put Your Artist Name: alan_warker, armin_van_buuren, avicii, billie_eilish, celine_dion, drake, green_day, halsey, Lady_Gaga, linkin_park, queen, taylor_swift ')
+                print(main(artist))
+            elif choice == 'texts':
+                print(mainw())
+            else:
+                quit()
+        else:
+            quit()
