82438f18a3f2f0b967505cc1b01de34c3b1efc2a
Marvin Kamau <stallionofoz@gmail.com>
2022-11-19 17:51:08 +0300

    Modified compose and graph template.py

--- compose_template.py
+++ compose_template.py
@@ -1,33 +1,69 @@
-"""
-Empty Compose Template to implement :D
-
-YouTube Kylie Ying: https://www.youtube.com/ycubed 
-Twitch KylieYing: https://www.twitch.tv/kylieying 
-Twitter @kylieyying: https://twitter.com/kylieyying 
-Instagram @kylieyying: https://www.instagram.com/kylieyying/ 
-Website: https://www.kylieying.com
-Github: https://www.github.com/kying18 
-Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5EUb3gzmlXIdsJkPs?si=qGc4ubKRRYmPHAJAIrCxVQ 
-"""
-
-import os
+import os 
 import re
 import string
 import random
-
-from graph import Graph, Vertex
+from graph import Graph,Vertex
 
 def get_words_from_text(text_path):
-    pass
+    with open(text_path,'rb') as file:
+        text = file.read().decode("utf-8")
+
+
+        # remove [verse 1: artist]
+        # include the following line if you are doing song lyrics
+        # text = re.sub(r'\[(.+)\]', ' ', text)
+
+        text = ' '.join(text.split())
+        text = text.lower()
+        text = text.translate(str.maketrans('','',string.punctuation))
+    
+    words = text.split()
+
+    words = words[:1000]
+
+    return words
 
 def make_graph(words):
-    pass
+    g = Graph()
+    prev_word = None
+    # for each word
+    for word in words:
+        # check that word is in graph, and if not then add it
+        word_vertex = g.get_vertex(word)
 
-def compose(g, words, length=50):
-    pass
+        # if there was a previous word, then add an edge if does not exist
+        # if exists, increment weight by 1
+        if prev_word:  # prev word should be a Vertex
+            # check if edge exists from previous word to current word
+            prev_word.increment_edge(word_vertex)
+
+        prev_word = word_vertex
+    
+    g.generate_probability_mappings()
+
+    return g
+
+def compose(g,words,length=50):
+    composition = []
+    word = g.get_vertex(random.choice(words))
+    for _ in range(length):
+        composition.append(word.value)
+        word = g.get_next_word(word)
+
+    return composition
+
 
 def main():
-    pass
+    words = get_words_from_text('texts/hp_sorcerer_stone.txt')
+
+    # for song in os.listdir('songs/{}'.format(artist)):
+        # if song == '.DS_Store':
+        #     continue
+        # words.extend(get_words_from_text('songs/{artist}/{song}'.format(artist=artist, song=song)))
+    
+    g = make_graph(words)
+    composition = compose(g,words,100)
+    print(' '.join(composition))
 
 
 if __name__ == '__main__':
--- graph_template.py
+++ graph_template.py
@@ -1,55 +1,53 @@
-
-"""
-Empty Graph Template to implement :D
-
-YouTube Kylie Ying: https://www.youtube.com/ycubed 
-Twitch KylieYing: https://www.twitch.tv/kylieying 
-Twitter @kylieyying: https://twitter.com/kylieyying 
-Instagram @kylieyying: https://www.instagram.com/kylieyying/ 
-Website: https://www.kylieying.com
-Github: https://www.github.com/kying18 
-Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5EUb3gzmlXIdsJkPs?si=qGc4ubKRRYmPHAJAIrCxVQ 
-"""
-
 import random
 
 class Vertex(object):
     def __init__(self, value):
-        pass
+        self.value = value
+        self.adjacent = {}  # nodes that it points to
+        self.neighbors = []
+        self.neighbors_weights = []
+
+    def __str__(self):
+        return self.value + ' '.join([node.value for node in self.adjacent.keys()])
 
     def add_edge_to(self, vertex, weight=0):
-        pass
+        self.adjacent[vertex] = weight
 
     def increment_edge(self, vertex):
-        pass
+        self.adjacent[vertex] = self.adjacent.get(vertex, 0) + 1
 
     def get_adjacent_nodes(self):
-        pass
+        return self.adjacent.keys()
 
     # initializes probability map
     def get_probability_map(self):
-        pass
+        for (vertex, weight) in self.adjacent.items():
+            self.neighbors.append(vertex)
+            self.neighbors_weights.append(weight)
 
     def next_word(self):
-        pass
+        return random.choices(self.neighbors, weights=self.neighbors_weights)[0]
 
 
 
 class Graph(object):
     def __init__(self):
-        pass
+        self.vertices = {}
 
     def get_vertex_values(self):
-        pass
+        return set(self.vertices.keys())
 
     def add_vertex(self, value):
-        pass
+        self.vertices[value] = Vertex(value)
 
     def get_vertex(self, value):
-        pass
+        if value not in self.vertices:
+            self.add_vertex(value)
+        return self.vertices[value]
 
     def get_next_word(self, current_vertex):
-        pass
+        return self.vertices[current_vertex.value].next_word()
 
     def generate_probability_mappings(self):
-        pass
+        for vertex in self.vertices.values():
+            vertex.get_probability_map()
